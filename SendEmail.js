482dd6dbbe1ef4bec1b5991e168098267388a385
Bobby Chien <bobby.chien@gmail.com>
2017-07-11 23:32:43 +0800

    Fixed bugs in dashboard title.

--- SendEmail.js
+++ SendEmail.js
@@ -1,8 +1,7 @@
-function testSendStatusEmail()
-{
+function testSendStatusEmail() {
 
-      // Get Email Body
-  var subject = "Update: TDC Weekly Status - "+ today.toDateString();
+  // Get Email Body
+  var subject = "Update: TDC Weekly Status - " + today.toDateString();
 
   // Send to single email
   MailApp.sendEmail({
@@ -17,7 +16,7 @@ function testSendStatusEmail()
 function SendStatusEmail() {
 
   // Get Email Body
-  var subject = "TDC Weekly Bug Status Update - "+ today.toDateString();
+  var subject = "TDC Weekly Bug Status Update - " + today.toDateString();
   var emailBody = composeNewEmailBody();
 
   var startRow = 2;  // First row of data to process
@@ -39,7 +38,7 @@ function SendStatusEmail() {
     if (email == '')
       break;
     else {
-      email = [ "\"", name, "\" <", email, ">"];
+      email = ["\"", name, "\" <", email, ">"];
       emails.push(email.join(""));
     }
   }
@@ -63,10 +62,16 @@ function composeNewEmailBody() {
   var myEmail = Session.getActiveUser().getEmail();
 
   // Get version information
+  var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+  var releaseDate = (new Date(dashboardSheet.getRange('C2').getValue() - tzoffset)).toISOString().slice(0,10);
+  var releaseWeek = [GetOrdinalNumberOf(dashboardSheet.getRange('E2').getValue()),
+                          dashboardSheet.getRange('D2').getValue()].join(' / ')
+
+  // replace variable in template
   template.currentVersion = dashboardSheet.getRange('B2').getValue();
-  template.releaseDate = dashboardSheet.getRange('C2').getValue().toISOString().slice(0,10);
-  template.releaseWeek = dashboardSheet.getRange('D2').getValue();
-  template.daysToGo = dashboardSheet.getRange('E2').getValue();
+  template.releaseDate = releaseDate;
+  template.releaseWeek = releaseWeek;
+  template.daysToGo = dashboardSheet.getRange('F2').getValue();
   template.FirefoxWeeklyCount = dashboardSheet.getRange('F2').getValue();
   template.AndroidWeeklyCount = dashboardSheet.getRange('G2').getValue();
 
@@ -75,4 +80,19 @@ function composeNewEmailBody() {
   var emailBody = template.evaluate().getContent();
   Logger.log(emailBody);
   return emailBody;
+}
+
+function GetOrdinalNumberOf(number) {
+  var j = number % 10,
+    k = number % 100;
+  if (j == 1 && k != 11) {
+    return number + "st";
+  }
+  if (j == 2 && k != 12) {
+    return number + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return number + "rd";
+  }
+  return number + "th";
 }
